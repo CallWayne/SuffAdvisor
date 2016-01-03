ba91397bc8274a77fa213d99824f53690c2a553b
CallWayne <schlueter.niklas@gmail.com>
2016-01-03 11:03:40 +0100

    Kommentar
    hinzufügen möglich, löschen noch nicht

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,39 @@ class CommentsController < ApplicationController
     redirect_to pub_path(@pub)
   end
 
+  def create
+    @event = Event.find(params[:event_id])
+    @comment = @event.comments.create(comment_params)
+    redirect_to event_path(@event)
+  end
+
+  def create
+    @discothek = Discothek.find(params[:discothek_id])
+    @comment = @discothek.comments.create(comment_params)
+    redirect_to discothek_path(@discothek)
+  end
+
+  def destroy
+    @pub = Pub.find(params[:pub_id])
+    @comment = @pub.comments.find(params[:id])
+    @comment.destroy
+    redirect_to pub_path(@pub)
+  end
+
+  def destroy
+    @event = Event.find(params[:event_id])
+    @comment = @event.comments.find(params[:id])
+    @comment.destroy
+    redirect_to event_path(@event)
+  end
+
+  def destroy
+    @discothek = Discothek.find(params[:discothek_id])
+    @comment = @discothek.comments.find(params[:id])
+    @comment.destroy
+    redirect_to discothek_path(@discothek)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)

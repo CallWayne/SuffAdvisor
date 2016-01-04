6c0f5bf74dd83561b214e8faba9628e2eaeaf291
Josua Suren <josua.suren@web.de>
2016-01-04 16:40:04 +0100

    ratyrate

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   has_many :comments, as: :commentable
   validates :title, presence: true,
             length: { minimum: 5 }
+  ratyrate_rateable 'score'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104133808) do
+ActiveRecord::Schema.define(version: 20160104153236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comment_events", force: :cascade do |t|
-    t.string   "commenter"
-    t.text     "body"
-    t.integer  "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "average_caches", force: :cascade do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "avg",           null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
-
-  add_index "comment_events", ["event_id"], name: "index_comment_events_on_event_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.string   "commentable_type"
@@ -55,6 +54,14 @@ ActiveRecord::Schema.define(version: 20160104133808) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "overall_averages", force: :cascade do |t|
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "overall_avg",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "pubs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -63,6 +70,31 @@ ActiveRecord::Schema.define(version: 20160104133808) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "rates", force: :cascade do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "stars",         null: false
+    t.string   "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rates", ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type", using: :btree
+  add_index "rates", ["rater_id"], name: "index_rates_on_rater_id", using: :btree
+
+  create_table "rating_caches", force: :cascade do |t|
+    t.integer  "cacheable_id"
+    t.string   "cacheable_type"
+    t.float    "avg",            null: false
+    t.integer  "qty",            null: false
+    t.string   "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -82,5 +114,4 @@ ActiveRecord::Schema.define(version: 20160104133808) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "comment_events", "events"
 end

49d8cd49b42ceb19489165cd9f480e97ccb0a364
CallWayne <schlueter.niklas@gmail.com>
2016-01-04 19:42:59 +0100

    rating
    rating

--- app/models/discothek.rb
+++ app/models/discothek.rb
@@ -2,4 +2,5 @@ class Discothek < ActiveRecord::Base
   has_many :comments, as: :commentable
   validates :title, presence: true,
             length: { minimum: 5 }
+  ratyrate_rateable 'score'
 end

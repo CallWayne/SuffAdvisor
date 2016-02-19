fce6784a40cbe61594661cec722541f5cd2d410e
CallWayne <schlueter.niklas@gmail.com>
2016-02-19 17:08:33 +0100

    Heroku
    …

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require jquery.raty
 //= require ratyrate
 //= require underscore
+//= require gmaps/google
+//= require_tree .
 
 
